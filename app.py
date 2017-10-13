f248cba319dc425539c5c30593579c9119939cc5
Emre Saglam <emresaglam@gmail.com>
2017-10-12 20:18:33 -0700

    Added some coloring to HipChat messages It will change color according to the AQI Also added .idea to gitignore

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, url_for, request
-from werkzeug.datastructures import ImmutableMultiDict
 import geocoder
 import requests
 import json
@@ -7,38 +6,59 @@ import os
 
 app = Flask(__name__)
 
+
 def get_air_quality(zipcode):
-	g = geocoder.google(zipcode)
-	lat = g.json["lat"]
-	lon = g.json["lng"]
-	token = os.environ["BR_TOKEN"]
-	url = "https://api.breezometer.com/baqi/?lat={}&lon={}&fields=country_aqi,breezometer_description&key={}".format(lat,lon,token)
-	r = requests.get(url)
-	message = "Air quality index is: {} - {}".format(r.json()["country_aqi"], r.json()["breezometer_description"])
-	print message
-	return message
+    air_quality = {}
+    g = geocoder.google(zipcode)
+    print g.json["city"], g.json["state"]
+    lat = g.json["lat"]
+    lon = g.json["lng"]
+    token = os.environ["BR_TOKEN"]
+    url = "https://api.breezometer.com/baqi/?lat={}&lon={}&fields=country_aqi,breezometer_description,country_name&key={}".format(
+        lat, lon, token)
+    r = requests.get(url)
+    air_quality["location"] = "{}, {}".format(g.json["city"], g.json["state"])
+    air_quality["raw"] = r.json()
+    message = "Air quality index for {} is: {} - {}".format(air_quality["location"], r.json()["country_aqi"], r.json()["breezometer_description"])
+    air_quality["message"] = message
+    return air_quality
+
 
 @app.route("/")
 def api_root():
-	return 'Oh hai!'
+    return "Oh hai! This is an API endpoint and this is not the URL you're looking for :)"
 
-@app.route('/aqi/', methods=["POST"])
-def aqi():
-	data = request.get_json(force=True,silent=False)
-	print data
-	zipcode = data["item"]["message"]["message"]
-	print zipcode
-	zipcode = zipcode[4:]
-	message = get_air_quality(zipcode)
-	returned = {}
-	returned["color"] = "green"
-	returned["message"] = message
-	returned["notify"] = False
-	returned["message_format"] = "text"
-	returned_json = json.dumps(returned)
-	print returned_json
-	return returned_json
+@app.route('/aqi/<zipcode>', methods=["GET", "POST"])
+def aqi(zipcode):
+    color = "yellow"
+    if request.method == "POST":
+        data = request.get_json(force=True, silent=False)
+        print data
+        zipcode = data["item"]["message"]["message"]
+        print zipcode
+        zipcode = zipcode[4:]
 
-	
+    message = get_air_quality(zipcode)
+    print message
+    aqi = message["raw"]["country_aqi"]
+
+    if aqi < 50:
+        color = "green"
+    elif 50 < aqi < 150:
+        color = "yellow"
+    elif 150 < aqi < 250:
+        color = "red"
+    elif 250 < aqi < 1000:
+        color = "purple"
+
+    returned = {}
+    returned["color"] = color
+    returned["message"] = message["message"]
+    returned["notify"] = False
+    returned["message_format"] = "text"
+    returned_json = json.dumps(returned)
+    print returned_json
+    return returned_json
+
 if __name__ == '__main__':
     app.run()
